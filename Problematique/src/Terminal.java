c71f0fd0c253e897072f922d2746d4a82455b839
Caroleannv <43645688+Caroleannv@users.noreply.github.com>
2020-04-11 11:02:13 -0400

    Autres classe pour analyse lexicale

---
+++ Problematique/src/Terminal.java
@@ -0,0 +1,18 @@
+/** @author Ahmed Khoumsi */
+
+/** Cette classe identifie les terminaux reconnus et retournes par
+ *  l'analyseur lexical
+ */
+public class Terminal {
+    String chaine;
+//  ....
+
+
+    /** Un ou deux constructeurs (ou plus, si vous voulez)
+     *   pour l'initalisation d'attributs
+     */
+    public Terminal(String chaine) {   // arguments possibles
+        chaine = chaine;
+    }
+
+}
